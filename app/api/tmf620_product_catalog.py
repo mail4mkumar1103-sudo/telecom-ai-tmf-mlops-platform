f1d7380e6eec2486de882da375ef5e27ca964848
mkumar <mail4mkumar1103@gmail.com>
2026-04-30 10:37:36 +0530

    Week1-Day1: Added 50GB product

--- app/api/tmf620_product_catalog.py
+++ app/api/tmf620_product_catalog.py
@@ -4,6 +4,7 @@ router = APIRouter()
 	
 PRODUCTS = [	
     {"id": "DATA_10GB", "name": "10GB Pack", "price": 199},	
+    {"id": "DATA_50GB", "name": "50GB Pack", "price": 499},
     {"id": "VOICE_100", "name": "100 Min Pack", "price": 99}	
 ]	
 	
