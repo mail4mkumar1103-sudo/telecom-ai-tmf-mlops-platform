fa71ef20607c28218732ce07f46c0e47e4190477
mkumar <mail4mkumar1103@gmail.com>
2026-05-01 15:50:22 +0530

    changes done in jason code return.

--- app/api/tmf620_product_catalog.py
+++ app/api/tmf620_product_catalog.py
@@ -12,7 +12,8 @@ router = APIRouter()
 PRODUCTS = [	
     {"id": "DATA_10GB", "name": "10GB Pack", "price": 199},	
     {"id": "DATA_50GB", "name": "50GB Pack", "price": 499},
-    {"id": "VOICE_100", "name": "100 Min Pack", "price": 99}	
+    {"id": "VOICE_100", "name": "100 Min Pack", "price": 99},
+    {"id": "VOICE_50", "name": "50 Min Pack", "price": 49}	
 ]	
 	
 @router.get("/productOffering")	
@@ -23,8 +24,9 @@ def get_products():
 def get_product_by_id(product_id: str):
     for product in PRODUCTS:
         if product["id"] == product_id:
-            return product
-    raise HTTPException(status_code=404, detail="Product not found")
+           # return product
+            return {"status": "success", "data": {"id": product["id"], "name": product["name"], "price": product["price"]}}
+    raise HTTPException(status_code=404, detail= {"status": "error", "error": {"code": 404,"message": "Product not found" }})
 
 
 @router.put("/productOffering/{product_id}")
@@ -32,8 +34,9 @@ def update_product(product_id: str, updated_product: Product):
     for product in PRODUCTS:
         if product["id"] == product_id:
             product.update(updated_product.dict())
-            return product
-    raise HTTPException(status_code=404, detail="Product not found")
+           # return product
+            return {"status": "success", "data": {"id": product["id"], "name": product["name"], "price": product["price"]}}
+    raise HTTPException(status_code=404, detail= {"status": "error", "error": {"code": 404,"message": "Product not found" }})
 
 
 @router.delete("/productOffering/{product_id}")
@@ -41,8 +44,10 @@ def delete_product(product_id: str):
     for product in PRODUCTS:
         if product["id"] == product_id:
             PRODUCTS.remove(product)
-            return {"message": "Product deleted"}
-    raise HTTPException(status_code=404, detail="Product not found")    
+        #    return {"message": "Product deleted"}
+            return {"status": "success", "data": {"message": "Product deleted"}
+}
+    raise HTTPException(status_code=404, detail= {"status": "error", "error": {"code": 404,"message": "Product not found" }})  
 
 
 @router.post("/productOffering")
